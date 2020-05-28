46de42048f3c2106d0d6854986d607ae8a78acbb
Janssen Lima <janssenreislima@gmail.com>
2020-05-28 12:02:22 -0300

    Atualizando numeração das tarefas.

--- basic_deploy.sh
+++ basic_deploy.sh
@@ -16,7 +16,7 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(
 apt-get update -y
 apt-get install docker-ce -y
 
-# add ccount to the docker group and configure driver to use systemd
+# add account to the docker group and configure driver to use systemd
 usermod -aG docker vagrant
 cat > /etc/docker/daemon.json <<EOF
 {
@@ -37,7 +37,7 @@ systemctl enable docker >/dev/null 2>&1
 systemctl restart docker
 
 # Add sysctl settings
-echo "[TASK 6] Add sysctl settings"
+echo "[TASK 4] Add sysctl settings"
 cat >>/etc/sysctl.d/kubernetes.conf<<EOF
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
@@ -45,12 +45,12 @@ EOF
 sysctl --system >/dev/null 2>&1
 
 # Disable swap
-echo "[TASK 7] Disable and turn off SWAP"
+echo "[TASK 5] Disable and turn off SWAP"
 sed -i '/swap/d' /etc/fstab
 swapoff -a
 
 # Install apt-transport-https pkg
-echo "[TASK 8] Installing apt-transport-https pkg"
+echo "[TASK 6] Installing apt-transport-https pkg"
 apt-get update && apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
@@ -64,22 +64,22 @@ ls -ltr /etc/apt/sources.list.d/kubernetes.list
 apt-get update -y
 
 # Install Kubernetes
-echo "[TASK 9] Install Kubernetes kubeadm, kubelet and kubectl"
+echo "[TASK 7] Install Kubernetes kubeadm, kubelet and kubectl"
 apt-get install -y kubelet kubeadm kubectl
 
 # Start and Enable kubelet service
-echo "[TASK 10] Enable and start kubelet service"
+echo "[TASK 8] Enable and start kubelet service"
 systemctl enable kubelet >/dev/null 2>&1
 systemctl start kubelet >/dev/null 2>&1
 
 # Enable ssh password authentication for copy files between master and nodes
-echo "[TASK 11] Enable ssh password authentication"
+echo "[TASK 9] Enable ssh password authentication"
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 systemctl restart sshd
 
 # Set Root password
-echo "[TASK 12] Set root password"
+echo "[TASK 10] Set root password"
 echo -e "kubeadmin\nkubeadmin" | passwd root
 
 # Update vagrant user's bashrc file
